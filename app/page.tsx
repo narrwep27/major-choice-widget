bd2e18d4d743392d4027aae9113c4fd97b015828
narrwep27 <narrwep@gmail.com>
2023-11-16 23:14:54 -0500

    began building home page and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div>Major Choice Widget</div>
-    </main>
-  )
+    return (
+        <main className={styles.main}>
+            <div className={styles.widget}>
+                <h2>What's the right</h2>
+                <h2>degree program for you?</h2>
+                <p>Take the quiz to find out what</p>
+                <p>program is right for you.</p>
+                <button>Start</button>
+                <Image
+                    className={styles.widget_background}
+                    src="/static/home-screen.png"
+                    fill={true}
+                    alt="Home page background"
+                />
+            </div>
+        </main>
+    );
 }
