54c595efb64efc4c6d4a1e7c9b5db0fe1e085ec1
narrwep27 <narrwep@gmail.com>
2023-11-25 03:24:24 -0500

    fixed errors causing build failure

--- app/components/ui/degreeChoiceBtn.tsx
+++ app/components/ui/degreeChoiceBtn.tsx
@@ -35,6 +35,7 @@ export default function DegreeChoiceBtn({
             onClick={() =>
                 setScreen(
                     <AptitudeQuestionScreen
+                        key={degreeType}
                         degreeType={degreeType}
                         setScreen={setScreen}
                     />
